23f871a1bc30972a1915480fbcce2c6e3be0645b
Sierra <98716240+srraspa@users.noreply.github.com>
2025-04-17 10:34:15 -0400

    updated IZ and LIF training scripts

--- Spike Encoding/SNN-Training/lif.py
+++ Spike Encoding/SNN-Training/lif.py
@@ -1,7 +1,4 @@
-# SNN Training Using STDP Unsupervised Algorithm
-# Most code taken from: https://colab.research.google.com/github/jeshraghian/snntorch/blob/master/examples/tutorial_5_FCN.ipynb#scrollTo=QXZ6Tuqc9Q-l
-
-########################### IMPORTS ######################################
+# imports
 import snntorch as snn
 from snntorch import spikeplot as splt
 from snntorch import spikegen
@@ -16,42 +13,6 @@ import numpy as np
 import itertools
 import csv
 
-########################## NEURON MODEL ####################################
-# snntorch only has variation of LIF neurons, but we need Izhikevech model
-# code and numbers from Yihui's matlab sim
-class izhikevech(nn.Module):
-  def __init__(self, u0, h, k=0.4471817006977834, a=0.0032799410036917333, b=24.478421990208606, Vmin=-66.46563513097735,
-               d=50.0, C=38.0, Vr=-77.40291336465064, Vt=-44.90054428048817, Vpeak=15.489726771001997):
-      super(izhikevech, self).__init__()
-
-      # initialize parameters of IZ model
-      self.u = u0
-      self.v = Vr
-      self.k = k
-      self.a = a
-      self.b = b
-      self.Vmin = Vmin
-      self.d = d
-      self.C = C
-      self.Vr = Vr
-      self.Vt = Vt
-      self.Vpeak = Vpeak
-      self.h = h
-        
-  # the forward function is called each time we call izhikevech
-  def forward(self, v, u, I):
-    # calculate t+1 point
-    # v = mem potential, u = recovery var
-    if (v>= self.Vpeak):
-        v = self.Vmin
-        u = u + self.d
-        spk = 1
-    else:
-        v = v + self.h * ( self.k * ( v - self.Vr ) * ( v - self.Vt) - u + I)/self.C
-        u = u + self.h * ( self.a * ( self.b * ( v-self.Vr ) - u ) )
-        spk = 0
-    return spk, v, u
-
 ############################### DATA PARSING AND FORMATTING ################################
 # load and parse encoded data from Jeremy's sim
 def importcsv(file_path, output_arr):
@@ -60,12 +21,9 @@ def importcsv(file_path, output_arr):
     for row in csv_reader:
         output_arr.append(row)
 
+# TRAINING data -----------------------------------------------------------------------
 file_path='C:\\Users\\Sierra\\OneDrive - University of Maryland\\UMD\\Spring 2025\\ENEE408D\\EEG Simulation\\Spike Encoding\\spike_enc_output.csv'
 spks = []
-# with open(file_path, 'r') as file:
-#     csv_reader = csv.reader(file)
-#     for row in csv_reader:
-#         spks.append(row)
 importcsv(file_path, spks)
 spks[0] = [float(x) for x in spks[0]]
 spks[1] = [float(x) for x in spks[1]]
@@ -78,7 +36,7 @@ targets = [[],[]]
 for i in range(len(targetsRaw)):
    targets[0].append(float(targetsRaw[i][0]))
    targets[1].append(float(targetsRaw[i][1]))
-labels = [x for x in targets[1]]
+labels = [1. if x >= 2 else x for x in targets[1]]
 
 # parse blinks into windows
 blink_window = int(0.5/(spks[0][1] - spks[0][0])) # +/- num of indices around center of spike
@@ -114,11 +72,56 @@ for center in targets[0]:
         break
         # this creates an array where each index is a singular blink with window 'blink_window'
 
-# TODO create digital signal from stem spikes to represent actual spike inputs
-# actually, I don't think I need to do this....
-# snn_clk = 1e6
-# period = 1./snn_clk
-# input_window = np.linspace(0, 1, 1/period)
+# TEST data--------------------------------------------------------------------------
+file_path='C:\\Users\\Sierra\\OneDrive - University of Maryland\\UMD\\Spring 2025\\ENEE408D\\EEG Simulation\\Spike Encoding\\spike_enc_output_test.csv'
+spks_test = []
+importcsv(file_path, spks_test)
+spks_test[0] = [float(x) for x in spks_test[0]]
+spks_test[1] = [float(x) for x in spks_test[1]]
+file_path = 'EEG-EyeBlinks/EEG-IO/S14_labels.csv'
+targetsRaw = []
+importcsv(file_path, targetsRaw) # targets starts at index 3
+# reformat target
+targetsRaw = targetsRaw[3:-1]
+targets_test = [[],[]]
+for i in range(len(targetsRaw)):
+   targets_test[0].append(float(targetsRaw[i][0]))
+   targets_test[1].append(float(targetsRaw[i][1]))
+labels_test = [1. if x >= 2 else x for x in targets_test[1]]
+
+# parse blinks into windows
+blink_window_test = int(0.5/(spks_test[0][1] - spks_test[0][0])) # +/- num of indices around center of spike
+blink_sets_test = [[], []]
+spks_pos_test = [[], []]
+spks_neg_test = [[], []]
+tmp_pos = []
+tmp_neg = []
+data_test = []
+err = 0.002
+for center in targets_test[0]:
+   for i, s in enumerate(spks_test[0]):
+    if (i >= blink_window_test and i <= len(spks_test[0])-blink_window_test) and (s == center or (s <= center+err and s >= center-err)):
+        # total inputs 
+        blink_sets_test[0].append(spks_test[0][i-blink_window_test:i+blink_window_test]) # time
+        blink_sets_test[1].append(spks_test[1][i-blink_window_test:i+blink_window_test]) # spikes
+        # separate into pos and neg spike inputs
+        spks_pos_test[0].append(spks_test[0][i-blink_window_test:i+blink_window_test]) # time
+        spks_neg_test[0].append(spks_test[0][i-blink_window_test:i+blink_window_test]) # time
+        for x in spks_test[1][i-blink_window_test:i+blink_window_test]:
+            y = x
+            if x < 0:
+              x = 0.0
+            tmp_pos.append(x)
+            if y > 0:
+               y = 0.0
+            tmp_neg.append(y)
+        spks_pos_test[1].append(tmp_pos) # spikes
+        spks_neg_test[1].append(tmp_neg) # spikes
+        data_test.append([tmp_pos, tmp_neg]) # I don't think I need to include time right? <---------------------?
+        tmp_pos = []
+        tmp_neg = []
+        break
+        # this creates an array where each index is a singular blink with window 'blink_window'
 
 # now convert the data into a torch.Tensor object (ugh.)
 class CustomDataset(Dataset):
@@ -137,24 +140,37 @@ class CustomDataset(Dataset):
     
 #batch_size = len(data)
 batch_size = 30
-data = np.array(data).reshape(blink_window*2,batch_size, 2)
-dataset = CustomDataset(data, labels)
+# Convert to torch tensor and rearrange dimensions
+data_tensor = torch.tensor(data, dtype=torch.float)  # shape: [30, 2, 256]
+data_tensor = data_tensor.permute(2, 0, 1)  # now shape: [256, 30, 2]
+labels_tensor = torch.tensor(labels, dtype=torch.long)  # shape: [30]
+from torch.utils.data import TensorDataset
+dataset = TensorDataset(data_tensor.permute(1, 0, 2), labels_tensor)  # [batch, time, input]
 train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+# Convert to torch tensor and rearrange dimensions
+data_tensor_test = torch.tensor(data_test, dtype=torch.float)  # shape: [30, 2, 256]
+data_tensor_test = data_tensor_test.permute(2, 0, 1)  # now shape: [256, 30, 2]
+labels_tensor_test = torch.tensor(labels_test, dtype=torch.long)  # shape: [30]
+dataset_test = TensorDataset(data_tensor_test.permute(1, 0, 2), labels_tensor_test)  # [batch, time, input]
+test_loader = DataLoader(dataset_test, batch_size=batch_size, shuffle=True, drop_last=True)
+
+# data = np.array(data).reshape(blink_window*2,batch_size, 2)
+# dataset = CustomDataset(data, labels)
+# train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 # I don't know what to do about the test_loader <-------------------------------------------------------------?
 
 #################################### NETWORK DEFINITIION #######################################
 dtype = torch.float
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
 # Network Architecture
 num_inputs = 2
 num_hidden = 4
 num_outputs = 2
 
 # Temporal Dynamics
-num_steps = blink_window*2
-u = 0
-v = 0
+num_steps = 256
+beta = 0.95
 
 # Define Network
 class Net(nn.Module):
@@ -163,28 +179,25 @@ class Net(nn.Module):
 
         # Initialize layers
         self.fc1 = nn.Linear(num_inputs, num_hidden)
-        self.iz1 = izhikevech(0, 0.1)
+        self.lif1 = snn.Leaky(beta=beta)
         self.fc2 = nn.Linear(num_hidden, num_outputs)
-        self.iz2 = izhikevech(0, 0.1)
+        self.lif2 = snn.Leaky(beta=beta)
 
     def forward(self, x):
 
         # Initialize hidden states at t=0
-        mem1 = self.iz1.v
-        mem2 = self.iz2.v
-        u1 = self.iz1.u
-        u2 = self.iz2.u
-
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
         
         # Record the final layer
         spk2_rec = []
         mem2_rec = []
 
         for step in range(num_steps):
-            cur1 = self.fc1(x[step]) # data shape: [time, batch size, inputs] = 256 x 30 x 2
-            spk1, mem1, u1 = self.iz1(mem1, u1, cur1)
+            cur1 = self.fc1(x[step])
+            spk1, mem1 = self.lif1(cur1, mem1)
             cur2 = self.fc2(spk1)
-            spk2, mem2, u2 = self.iz2(mem2, u2, cur2)
+            spk2, mem2 = self.lif2(cur2, mem2)
             spk2_rec.append(spk2)
             mem2_rec.append(mem2)
 
@@ -193,12 +206,12 @@ class Net(nn.Module):
 # Load the network onto CUDA if available
 net = Net().to(device)
 
-###################################### TRAINING #########################################
 # pass data into the network, sum the spikes over time
 # and compare the neuron with the highest number of spikes
 # with the target
 
 def print_batch_accuracy(data, targets, train=False):
+    #output, _ = net(data.view(batch_size, -1))
     output, _ = net(data)
     _, idx = output.sum(dim=0).max(1)
     acc = np.mean((targets == idx).detach().cpu().numpy())
@@ -220,7 +233,6 @@ def train_printer(
     print("\n")
 
 loss = nn.CrossEntropyLoss()
-
 optimizer = torch.optim.Adam(net.parameters(), lr=5e-4, betas=(0.9, 0.999))
 
 num_epochs = 1
@@ -235,12 +247,14 @@ for epoch in range(num_epochs):
 
     # Minibatch training loop
     for data, targets in train_batch:
-        data = data.to(device)
+        #data = data.to(device)
+        data = data.permute(1, 0, 2).to(device)
         targets = targets.to(device)
 
         # forward pass
         net.train()
-        spk_rec, mem_rec = net(data.view(batch_size, -1))
+        #spk_rec, mem_rec = net(data.view(batch_size, -1))
+        spk_rec, mem_rec = net(data)
 
         # initialize the loss & sum over time
         loss_val = torch.zeros((1), dtype=dtype, device=device)
@@ -255,5 +269,41 @@ for epoch in range(num_epochs):
         # Store loss history for future plotting
         loss_hist.append(loss_val.item())
 
-    counter += 1
-    iter_counter +=1
+        # Test set
+        with torch.no_grad():
+            net.eval()
+            test_data, test_targets = next(iter(test_loader))
+            #test_data = test_data.to(device)
+            test_data = test_data.permute(1, 0, 2).to(device)
+            test_targets = test_targets.to(device)
+
+            # Test set forward pass
+            test_spk, test_mem = net(test_data)
+
+            # Test set loss
+            test_loss = torch.zeros((1), dtype=dtype, device=device)
+            for step in range(num_steps):
+                test_loss += loss(test_mem[step], test_targets)
+            test_loss_hist.append(test_loss.item())
+
+            # Print train/test loss/accuracy
+            if counter % 50 == 0:
+                train_printer(
+                    data, targets, epoch,
+                    counter, iter_counter,
+                    loss_hist, test_loss_hist,
+                    test_data, test_targets)
+            counter += 1
+            iter_counter +=1
+
+
+
+# Plot Loss
+fig = plt.figure(facecolor="w", figsize=(10, 5))
+plt.plot(loss_hist)
+#plt.plot(test_loss_hist)
+plt.title("Loss Curves")
+#plt.legend(["Train Loss", "Test Loss"])
+plt.xlabel("Iteration")
+plt.ylabel("Loss")
+plt.show()
